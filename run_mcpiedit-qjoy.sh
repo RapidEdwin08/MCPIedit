b391fbe4d572556d423d500ee2033295516af15a
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2026-08-11 08:08:36 -0400

    Add files via upload

--- run_mcpiedit-qjoy.sh
+++ run_mcpiedit-qjoy.sh
@@ -26,15 +26,10 @@ rm /tmp/qjoypad.pid > /dev/null 2>&1
 echo "qjoypad "$qjoyLAYOUT" &" >> /dev/shm/runcommand.info
 qjoypad "$qjoyLAYOUT" &
 
-if [[ ! -d $HOME/.minecraft-pi ]]; then mkdir -p $HOME/.minecraft-pi; fi
-pushd $HOME/.minecraft-pi
-
 # Run mcpiedit in isolated virtual environment
-source envmcpiedit/bin/activate
+source $HOME/.mcpiedit/envmcpiedit/bin/activate
 $HOME/.mcpiedit/mcpiedit
 deactivate
-
-popd
 
 # Kill qjoypad
 pkill -15 qjoypad > /dev/null 2>&1; rm /tmp/qjoypad.pid > /dev/null 2>&1
--- run_mcpiedit.sh
+++ run_mcpiedit.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # https://github.com/RapidEdwin08/
 
-if [[ ! -d $HOME/.minecraft-pi ]]; then mkdir -p $HOME/.minecraft-pi; fi
-pushd $HOME/.minecraft-pi
-
 # Run mcpiedit in isolated virtual environment
-source envmcpiedit/bin/activate
+source $HOME/.mcpiedit/envmcpiedit/bin/activate
 $HOME/.mcpiedit/mcpiedit
 deactivate
-
-popd
